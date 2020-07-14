e198c20a13c674bf75821ca713faf047da0b5316
Charlie May <camiv95@gmail.com>
2020-07-14 13:49:43 -0500

    Update the verision and attempt to update the test PyPI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata6", # the name that you will install via pip
-    version="1.0",
+    version="1.1",
     author="Charlie May",
     author_email="camiv95@gmail.com",
     description="contains a function that enlarges a number by 100x",
