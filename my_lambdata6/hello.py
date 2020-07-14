521e6310b3dc6fd4c6d9b5bfa80fae94db755127
Charlie May <camiv95@gmail.com>
2020-07-14 13:31:42 -0500

    Prepare to distrubute the package

--- my_lambdata6/hello.py
+++ my_lambdata6/hello.py
@@ -1,8 +1,12 @@
 
-#from my_mod import enlarge
+from pandas import DataFrame
 
 from my_lambdata6.my_mod import enlarge
 
 print('hello')
 
 print(enlarge(8))
+
+
+df = DataFrame({'state': ['CT', 'CO', 'CA', 'TX']})
+print(df.head())
---
+++ setup.py
@@ -0,0 +1,20 @@
+
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lambdata6", # the name that you will install via pip
+    version="1.0",
+    author="Charlie May",
+    author_email="camiv95@gmail.com",
+    description="contains a function that enlarges a number by 100x",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    license="MIT",
+    url="https://github.com/charlie-may86/lambdata-pt6",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
